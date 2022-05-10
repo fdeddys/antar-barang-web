4b20a2315ea94f6bfd50541ba8010e14cbf8ea94
deddy <fransiscus.deddy@gmail.com>
2022-05-10 08:07:40 +0700

    update config

--- src/environments/environment.cloud.ts
+++ src/environments/environment.cloud.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    APIEndpoint: 'http://156.67.214.228:7777/',
+    APIEndpoint: 'http://156.67.214.228:8888/',
     environmentName: 'cloud',
   };
